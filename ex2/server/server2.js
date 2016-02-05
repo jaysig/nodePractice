1e2a671086344f95f7100388868845773bc29e10
Jaysig <jasontsigmon@gmail.com>
2016-02-05 18:02:00 -0500

    corrections for ex2

--- ex2/server/server2.js
+++ ex2/server/server2.js
@@ -16,6 +16,7 @@ app.use(express.static('client'));
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
 app.use(bodyParser.urlencoded({extended: true}));
+//Allows req.body to be accessed for data
 app.use(bodyParser.json());
 
 
@@ -29,28 +30,50 @@ app.get('/lions', function(req,res){
 });
 //returns one lion based on an id
 app.get('/lions/'+id,function(req,res){
-  var lion = lions[id];
-  res.json(lion);
+  //Find in the lions array an object whose id matches the searched for id
+  var lion = _.find(lions, {id: req.params.id});
+  res.json(lion || {});
 });
 //Add a lion
 app.post('/lions',function(req,res){
-  var lion = req.body.lion;
+  //just need to send the req.body
+  var lion = req.body;
+  //Increment the id for a new lion
+  id++;
+  lion.id = id + '';
+
   lions.push(lion);
   res.send(lion);
 });
 //Update a lion
 app.put('/lions',function(req,res){
-  var lion = req.body.lion;
-  var lionIndex = lions.indexOf(lion);
-  lions[lionIndex] = lion;
-  res.send(lion);
+  var update = req.body;
+  //clear the id that was sent in the req.body because we want to use an existing object
+  if(update.id){
+    delete update.id;
+  }
+  var lion = _.find(lions, {id: req.params.id});
+  if(!lions[lion]) {
+    res.send();
+  } else {
+    //merges two objects
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
 });
+
 //Delete a lion
-app.destroy('/lions',function(req,res){
-  var lion = req.body.lion;
-  var lionIndex = lions.indexOf(lion);
-  lions.splice(lionIndex,1);
-  res.send('lion removed');
+app.delete('/lions/:id',function(req,res){
+  
+  var lion = _.find(lions, {id: req.params.id});
+  if(!lions[lion]) {
+    res.send();
+  } else {
+    //merges two objects
+    var deletedLion = lions[lion];
+    lions.splice(lion,1);
+    res.json(updatedLion, 'lion removed');
+  }
 });
 
 app.listen(3000);
