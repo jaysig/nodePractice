03d872b6e2c653b65507fee5266e8a59eabe9385
Jaysig <jasontsigmon@gmail.com>
2016-02-04 19:03:53 -0500

    ex1 fixed

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-var fs = require('fs');
-var express = require('express');
 
+var express = require('express');
 var app = express();
 
 var jsonData = {count: 12, message: '12'};
 
 app.get('/', function(req,res){
-	res.json(jsonData);
+	//res.sendFile takes an absolute path to a file and
+	// sets the file mime type based on the file extname 
+	res.sendFile(__dirname + '/index.html', function(err){
+		if(err){
+			res.status(500).send(err);
+		}
+	});
 });
+
 app.get('/data', function(req,res){
 	res.json(jsonData);
 });
 
-app.listen(3000);
+var port = 3000;
+app.listen(port, function(){
+	console.log('listening on port', port);
+});
