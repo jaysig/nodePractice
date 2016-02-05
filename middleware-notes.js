39115188b68e517d111c7c17e1b3f1f9a0d59cab
Jaysig <jasontsigmon@gmail.com>
2016-02-05 18:28:35 -0500

    Middleware types

--- middleware-notes.js
+++ middleware-notes.js
@@ -1,2 +1,11 @@
 //Middleware is a function with access to the request object and the next() function
-//Stack of functions that wait for next() or to end
+//Stack of functions that wait for next() or a response
+
+/*  Types - Still functions
+ 3rd party - parser
+ Router Level
+ Application Level - app.use, app.all(do something on all requests)
+ Error-handling - function that takes (err, req, res, next)
+ Bulit-in - express.static 
+
+*/
